e7a8812ef7d0e2c77e427c82529e3ff2dc975656
Habtamu Petros <89646714+habte0904@users.noreply.github.com>
2025-05-13 19:45:24 +0300

    message

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-#exit on error
-
+# Exit on error
 set -o errexist
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-if [[$CREATE_SUPERUSER]];
-then
+
+if [[ $CREATE_SUPERUSER ]]; then
   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
 fi
